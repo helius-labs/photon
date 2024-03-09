d32d4c7d25f27a8aa50586c55ecdc7f8da110df6
Pedro Mantica <pedro@helius.xyz>
2024-03-09 01:50:58 +0000

    Move to top level directory

--- photon/tests/integration_tests/main.rs
+++
@@ -1,7 +0,0 @@
-// We move all tests into a single tests/integration_tests directory so that we have a single test
-// binary. This has a number of benefits:
-// 1. Faster compile time since we only have 1 binary.
-// 2. The ability to add global locks on the DB to prevent tests from interfering with each other.
-mod parsing_tests;
-mod persist_tests;
-mod utils;
