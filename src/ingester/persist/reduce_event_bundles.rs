14f2798eaf930bd5ea20e394523e718d3299f651
Pedro Mantica <pedro@helius.xyz>
2024-03-15 16:30:44 +0000

    Improve code

--- src/ingester/persist/reduce_event_bundles.rs
+++ src/ingester/persist/reduce_event_bundles.rs
@@ -1,6 +1,5 @@
 use light_merkle_tree_event::{ChangelogEvent, Changelogs, PathNode};
 use psp_compressed_pda::utxo::Utxo;
-use solana_sdk::clock::Slot;
 
 use crate::ingester::{error::IngesterError, parser::bundle::EventBundle};
 
@@ -41,58 +40,46 @@ pub async fn consolidate_event_bundle(
     let mut merged_bundle = MergedEventBundle::default();
 
     for event in events {
-        match event {
-            EventBundle::PublicTransactionEvent(e) => {
-                for in_utxo in e.in_utxos {
-                    merged_bundle.in_utxos.push(UtxoWithSlot {
-                        utxo: in_utxo,
-                        slot: e.slot as i64,
-                    });
-                }
+        let EventBundle::PublicTransactionEvent(e) = event;
+        merged_bundle
+            .in_utxos
+            .extend(e.in_utxos.into_iter().map(|utxo| UtxoWithSlot {
+                utxo,
+                slot: e.slot as i64,
+            }));
 
-                let path_updates = extract_path_updates(e.changelogs, e.slot);
+        let path_updates = extract_path_updates(&e.changelogs);
 
-                if e.out_utxos.len() != path_updates.len() {
-                    return Err(IngesterError::MalformedEvent {
-                        msg: format!(
-                            "Number of path updates did not match the number of output UTXOs (txn: {})",
-                            e.transaction,
-                        ),
-                    });
-                }
-
-                // TODO: Do batch inserts here
-                for i in 0..e.out_utxos.len() {
-                    let out_utxo = &e.out_utxos[i];
-                    let path = &path_updates[i];
-                    let tree = path.tree;
-                    let seq = path.seq;
-                    merged_bundle.out_utxos.push(EnrichedUtxo {
-                        utxo: UtxoWithSlot {
-                            utxo: out_utxo.clone(),
-                            slot: e.slot as i64,
-                        },
-                        tree,
-                        seq,
-                    });
-                }
-
-                merged_bundle
-                    .path_nodes
-                    .extend(path_updates.iter().flat_map(|p| {
-                        p.path.iter().map(move |node| EnrichedPathNode {
-                            // PathNode does not support cloning.
-                            node: PathNode {
-                                node: node.node.clone(),
-                                index: node.index,
-                            },
-                            slot: e.slot as i64,
-                            tree: p.tree,
-                            seq: p.seq,
-                        })
-                    }));
-            }
+        if e.out_utxos.len() != path_updates.len() {
+            return Err(IngesterError::MalformedEvent {
+                msg: format!(
+                    "Number of path updates did not match the number of output UTXOs (txn: {})",
+                    e.transaction,
+                ),
+            });
         }
+
+        for (out_utxo, path) in e.out_utxos.into_iter().zip(path_updates.iter()) {
+            merged_bundle.out_utxos.push(EnrichedUtxo {
+                utxo: UtxoWithSlot {
+                    utxo: out_utxo,
+                    slot: e.slot as i64,
+                },
+                tree: path.tree,
+                seq: path.seq,
+            });
+        }
+
+        merged_bundle
+            .path_nodes
+            .extend(path_updates.into_iter().flat_map(|p| {
+                p.path.into_iter().map(move |node| EnrichedPathNode {
+                    node,
+                    slot: e.slot as i64,
+                    tree: p.tree,
+                    seq: p.seq,
+                })
+            }));
     }
     Ok(merged_bundle)
 }
