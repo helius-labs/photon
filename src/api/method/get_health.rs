143683fcfbe2b6f2beff2b47b0b03cfa8776bfba
Pedro Mantica <pedro@helius.xyz>
2024-04-09 22:13:44 +0000

    Improve local development experience

--- src/api/method/get_health.rs
+++ src/api/method/get_health.rs
@@ -2,12 +2,11 @@ use sea_orm::DatabaseConnection;
 
 use solana_client::nonblocking::rpc_client::RpcClient;
 
-
 use super::super::error::PhotonApiError;
 use super::utils::Context;
 
 // TODO: Make this an environment variable.
-const HEALTH_CHECK_SLOT_DISTANCE: u64 = 5;
+const HEALTH_CHECK_SLOT_DISTANCE: i64 = 5;
 
 // TODO: Make sure that get_health formatting matches the Solana RPC formatting.
 pub async fn get_health(
@@ -20,9 +19,9 @@ pub async fn get_health(
         .await
         .map_err(|e| PhotonApiError::UnexpectedError(format!("RPC error: {}", e)))?;
 
-    let slots_behind = slot - context.slot;
+    let slots_behind = slot as i64 - context.slot as i64;
     if slots_behind > HEALTH_CHECK_SLOT_DISTANCE {
-        return Err(PhotonApiError::StaleSlot(slots_behind));
+        return Err(PhotonApiError::StaleSlot(slots_behind as u64));
     }
     Ok("ok".to_string())
 }
--- src/api/rpc_server.rs
+++ src/api/rpc_server.rs
@@ -1,4 +1,4 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, sync::Arc};
 
 use hyper::Method;
 use jsonrpsee::{
@@ -6,16 +6,18 @@ use jsonrpsee::{
     RpcModule,
 };
 use log::debug;
+use tokio::sync::Mutex;
 use tower_http::cors::{Any, CorsLayer};
 
-use super::api::PhotonApi;
-use super::method::{
-    get_compressed_accounts_by_owner::GetCompressedAccountsByOwnerRequest,
-    get_multiple_compressed_accounts::GetMultipleCompressedAccountsRequest,
-    utils::CompressedAccountRequest,
-};
+use crate::ingester::indexer::Indexer;
 
-pub async fn run_server(api: PhotonApi, port: u16) -> Result<ServerHandle, anyhow::Error> {
+use super::api::PhotonApi;
+
+pub async fn run_server(
+    api: PhotonApi,
+    port: u16,
+    indexer: Option<Arc<Mutex<Indexer>>>,
+) -> Result<ServerHandle, anyhow::Error> {
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     let cors = CorsLayer::new()
         .allow_methods([Method::POST, Method::GET])
@@ -29,29 +31,45 @@ pub async fn run_server(api: PhotonApi, port: u16) -> Result<ServerHandle, anyho
         .set_middleware(middleware)
         .build(addr)
         .await?;
-    let rpc_module = build_rpc_module(api)?;
+    let rpc_module = build_rpc_module(ApiAndIndexer { api, indexer })?;
     server.start(rpc_module).map_err(|e| anyhow::anyhow!(e))
 }
 
-pub fn build_rpc_module(contract: PhotonApi) -> Result<RpcModule<PhotonApi>, anyhow::Error> {
-    let mut module = RpcModule::new(contract);
+struct ApiAndIndexer {
+    api: PhotonApi,
+    indexer: Option<Arc<Mutex<Indexer>>>,
+}
+
+async fn conditionally_index_latest_blocks(indexer: &Option<Arc<Mutex<Indexer>>>) {
+    if let Some(indexer) = indexer {
+        indexer.lock().await.index_latest_blocks(None).await;
+    }
+}
+
+fn build_rpc_module(
+    api_and_indexer: ApiAndIndexer,
+) -> Result<RpcModule<ApiAndIndexer>, anyhow::Error> {
+    let mut module = RpcModule::new(api_and_indexer);
 
     module.register_async_method("liveness", |_rpc_params, rpc_context| async move {
         debug!("Checking Liveness");
-        rpc_context.liveness().await.map_err(Into::into)
+        let ApiAndIndexer { api, .. } = rpc_context.as_ref();
+        api.liveness().await.map_err(Into::into)
     })?;
 
     module.register_async_method("readiness", |_rpc_params, rpc_context| async move {
         debug!("Checking Readiness");
-        rpc_context.readiness().await.map_err(Into::into)
+        let ApiAndIndexer { api, .. } = rpc_context.as_ref();
+        api.readiness().await.map_err(Into::into)
     })?;
 
     module.register_async_method(
         "getCompressedAccount",
         |rpc_params, rpc_context| async move {
+            let ApiAndIndexer { api, indexer } = rpc_context.as_ref();
+            conditionally_index_latest_blocks(indexer).await;
             let payload = rpc_params.parse()?;
-            rpc_context
-                .get_compressed_account(payload)
+            api.get_compressed_account(payload)
                 .await
                 .map_err(Into::into)
         },
@@ -60,9 +78,10 @@ pub fn build_rpc_module(contract: PhotonApi) -> Result<RpcModule<PhotonApi>, any
     module.register_async_method(
         "getCompressedAccountProof",
         |rpc_params, rpc_context| async move {
+            let ApiAndIndexer { api, indexer } = rpc_context.as_ref();
+            conditionally_index_latest_blocks(indexer).await;
             let payload = rpc_params.parse()?;
-            rpc_context
-                .get_compressed_account_proof(payload)
+            api.get_compressed_account_proof(payload)
                 .await
                 .map_err(Into::into)
         },
@@ -71,9 +90,10 @@ pub fn build_rpc_module(contract: PhotonApi) -> Result<RpcModule<PhotonApi>, any
     module.register_async_method(
         "getMultipleCompressedAccountProofs",
         |rpc_params, rpc_context| async move {
+            let ApiAndIndexer { api, indexer } = rpc_context.as_ref();
+            conditionally_index_latest_blocks(indexer).await;
             let payload = rpc_params.parse()?;
-            rpc_context
-                .get_multiple_compressed_account_proofs(payload)
+            api.get_multiple_compressed_account_proofs(payload)
                 .await
                 .map_err(Into::into)
         },
@@ -82,9 +102,10 @@ pub fn build_rpc_module(contract: PhotonApi) -> Result<RpcModule<PhotonApi>, any
     module.register_async_method(
         "getCompressedTokenAccountsByOwner",
         |rpc_params, rpc_context| async move {
+            let ApiAndIndexer { api, indexer } = rpc_context.as_ref();
+            conditionally_index_latest_blocks(indexer).await;
             let payload = rpc_params.parse()?;
-            rpc_context
-                .get_compressed_token_accounts_by_owner(payload)
+            api.get_compressed_token_accounts_by_owner(payload)
                 .await
                 .map_err(Into::into)
         },
@@ -93,9 +114,10 @@ pub fn build_rpc_module(contract: PhotonApi) -> Result<RpcModule<PhotonApi>, any
     module.register_async_method(
         "getCompressedTokenAccountsByDelegate",
         |rpc_params, rpc_context| async move {
+            let ApiAndIndexer { api, indexer } = rpc_context.as_ref();
+            conditionally_index_latest_blocks(indexer).await;
             let payload = rpc_params.parse()?;
-            rpc_context
-                .get_compressed_token_accounts_by_delegate(payload)
+            api.get_compressed_token_accounts_by_delegate(payload)
                 .await
                 .map_err(Into::into)
         },
@@ -104,9 +126,10 @@ pub fn build_rpc_module(contract: PhotonApi) -> Result<RpcModule<PhotonApi>, any
     module.register_async_method(
         "getCompressedTokenAccountBalance",
         |rpc_params, rpc_context| async move {
-            let payload = rpc_params.parse::<CompressedAccountRequest>()?;
-            rpc_context
-                .get_compressed_token_account_balance(payload)
+            let ApiAndIndexer { api, indexer } = rpc_context.as_ref();
+            conditionally_index_latest_blocks(indexer).await;
+            let payload = rpc_params.parse()?;
+            api.get_compressed_token_account_balance(payload)
                 .await
                 .map_err(Into::into)
         },
@@ -115,28 +138,32 @@ pub fn build_rpc_module(contract: PhotonApi) -> Result<RpcModule<PhotonApi>, any
     module.register_async_method(
         "getCompressedBalance",
         |rpc_params, rpc_context| async move {
-            let payload = rpc_params.parse::<CompressedAccountRequest>()?;
-            rpc_context
-                .get_compressed_balance(payload)
+            let ApiAndIndexer { api, indexer } = rpc_context.as_ref();
+            conditionally_index_latest_blocks(indexer).await;
+            let payload = rpc_params.parse()?;
+            api.get_compressed_balance(payload)
                 .await
                 .map_err(Into::into)
         },
     )?;
 
     module.register_async_method("getHealth", |_rpc_params, rpc_context| async move {
-        rpc_context.get_health().await.map_err(Into::into)
+        rpc_context.api.get_health().await.map_err(Into::into)
     })?;
 
     module.register_async_method("getSlot", |_rpc_params, rpc_context| async move {
-        rpc_context.get_slot().await.map_err(Into::into)
+        let ApiAndIndexer { api, indexer } = rpc_context.as_ref();
+        conditionally_index_latest_blocks(indexer).await;
+        api.get_slot().await.map_err(Into::into)
     })?;
 
     module.register_async_method(
-        "getCompressedProgramAccounts",
+        "getCompressedAccountsByOwner",
         |rpc_params, rpc_context| async move {
-            let payload = rpc_params.parse::<GetCompressedAccountsByOwnerRequest>()?;
-            rpc_context
-                .get_compressed_accounts_by_owner(payload)
+            let ApiAndIndexer { api, indexer } = rpc_context.as_ref();
+            conditionally_index_latest_blocks(indexer).await;
+            let payload = rpc_params.parse()?;
+            api.get_compressed_accounts_by_owner(payload)
                 .await
                 .map_err(Into::into)
         },
@@ -145,9 +172,10 @@ pub fn build_rpc_module(contract: PhotonApi) -> Result<RpcModule<PhotonApi>, any
     module.register_async_method(
         "getMultipleCompressedAccounts",
         |rpc_params, rpc_context| async move {
-            let payload = rpc_params.parse::<GetMultipleCompressedAccountsRequest>()?;
-            rpc_context
-                .get_multiple_compressed_accounts(payload)
+            let ApiAndIndexer { api, indexer } = rpc_context.as_ref();
+            conditionally_index_latest_blocks(indexer).await;
+            let payload = rpc_params.parse()?;
+            api.get_multiple_compressed_accounts(payload)
                 .await
                 .map_err(Into::into)
         },
