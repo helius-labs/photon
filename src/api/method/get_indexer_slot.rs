990258ec7bfa3564ab8455ac6be7aaaf9ee0dda5
Pedro Mantica <pedro@helius.xyz>
2024-04-25 20:26:16 +0000

    Adjust

--- src/api/method/get_indexer_slot.rs
+++ src/api/method/get_indexer_slot.rs
@@ -3,6 +3,6 @@ use sea_orm::DatabaseConnection;
 use super::super::error::PhotonApiError;
 use super::utils::Context;
 
-pub async fn get_slot(conn: &DatabaseConnection) -> Result<u64, PhotonApiError> {
+pub async fn get_indexer_slot(conn: &DatabaseConnection) -> Result<u64, PhotonApiError> {
     Ok(Context::extract(conn).await?.slot)
 }
