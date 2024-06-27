3d7999301b9d5d6cac98e02e86e5d0a425a589a2
Pedro Mantica <pedro@helius.xyz>
2024-06-27 22:20:10 +0000

    Uncomment tests

--- tests/integration_tests/open_api_tests.rs
+++ tests/integration_tests/open_api_tests.rs
@@ -1,6 +1,6 @@
-// use photon_indexer::openapi::update_docs;
+use photon_indexer::openapi::update_docs;
 
-// #[test]
-// pub fn test_documentation_generation() {
-//     update_docs(true);
-// }
+#[test]
+pub fn test_documentation_generation() {
+    update_docs(true);
+}
