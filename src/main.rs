ba7c87cb9afeef0ee7bfa2f94859d0a47c862e0c
pmantica11 <151664502+pmantica11@users.noreply.github.com>
2024-07-08 11:25:16 -0600

    Specify default start slot for devnet (#124)

--- src/main.rs
+++ src/main.rs
@@ -193,8 +193,8 @@ async fn main() {
         CommitmentConfig::confirmed(),
     ));
 
-    info!("Starting indexer...");
     let is_localnet = args.rpc_url.contains("127.0.0.1");
+<<<<<<< Updated upstream
     // For localnet we can safely use a large batch size to speed up indexing.
     let max_concurrent_block_fetches = match args.max_concurrent_block_fetches {
         Some(max_concurrent_block_fetches) => max_concurrent_block_fetches,
@@ -206,18 +206,44 @@ async fn main() {
             }
         }
     };
+    let indexer = Indexer::new(
+        db_conn.clone(),
+        rpc_client.clone(),
+        is_localnet,
+        args.start_slot,
+        max_concurrent_block_fetches,
+    )
+    .await;
+    let indexer = Arc::new(Mutex::new(indexer));
+=======
+
     let mut indexer = None;
+>>>>>>> Stashed changes
 
     let indexer_handle = match args.disable_indexing {
         true => {
             info!("Indexing is disabled");
             None
         }
+<<<<<<< Updated upstream
+        false => Some(tokio::task::spawn(continously_run_indexer(indexer.clone()))),
+=======
         false => {
+            info!("Starting indexer...");
+            // For localnet we can safely use a large batch size to speed up indexing.
+            let max_concurrent_block_fetches = match args.max_concurrent_block_fetches {
+                Some(max_concurrent_block_fetches) => max_concurrent_block_fetches,
+                None => {
+                    if is_localnet {
+                        200
+                    } else {
+                        20
+                    }
+                }
+            };
             let indexer_instance = Indexer::new(
                 db_conn.clone(),
                 rpc_client.clone(),
-                is_localnet,
                 args.start_slot,
                 max_concurrent_block_fetches,
             )
@@ -227,6 +253,7 @@ async fn main() {
                 indexer.clone().unwrap(),
             )))
         }
+>>>>>>> Stashed changes
     };
 
     info!("Starting API server with port {}...", args.port);
@@ -238,7 +265,7 @@ async fn main() {
                 db_conn.clone(),
                 rpc_client.clone(),
                 args.prover_url,
-                indexer,
+                Some(indexer.clone()),
                 args.port,
             )
             .await,
