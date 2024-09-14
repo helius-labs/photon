1e5e65772181996d8b3e0174524e75c8d8ef693e
pmantica11 <151664502+pmantica11@users.noreply.github.com>
2024-09-14 11:12:00 -0400

    Add missing index (#204)

--- src/migration/mod.rs
+++ src/migration/mod.rs
@@ -4,6 +4,7 @@ mod m20220101_000001_init;
 mod m20240623_000002_init;
 mod m20240624_000003_init;
 mod m20240807_000004_init;
+mod m20240914_000005_init;
 mod model;
 
 pub struct Migrator;
@@ -16,6 +17,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20240623_000002_init::Migration),
             Box::new(m20240624_000003_init::Migration),
             Box::new(m20240807_000004_init::Migration),
+            Box::new(m20240914_000005_init::Migration),
         ]
     }
 }
