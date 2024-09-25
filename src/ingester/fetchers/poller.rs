a27981e80fae46db38b983dc49dbdd855734235c
pmantica11 <151664502+pmantica11@users.noreply.github.com>
2024-09-24 20:54:52 -0600

    Improve polling (#211)
    * Improve polling
    
    * Nits
    
    * Nits

--- src/ingester/fetchers/poller.rs
+++ src/ingester/fetchers/poller.rs
@@ -1,6 +1,7 @@
 use std::{
     cmp::max,
     collections::{BTreeMap, HashSet},
+    num::NonZeroUsize,
     sync::{
         atomic::{AtomicU64, Ordering},
         Arc,
@@ -12,6 +13,7 @@ use std::{
 use async_stream::stream;
 use cadence_macros::statsd_count;
 use futures::{stream::FuturesUnordered, StreamExt};
+use lru::LruCache;
 use solana_client::{
     client_error, nonblocking::rpc_client::RpcClient, rpc_config::RpcBlockConfig,
     rpc_request::RpcError,
@@ -30,7 +32,7 @@ use crate::{
 const SKIPPED_BLOCK_ERRORS: [i64; 2] = [-32007, -32009];
 const RETRIES: u64 = 3;
 const INFINITY: u64 = u64::MAX;
-const MAX_LOOK_AHEAD: u64 = 2;
+const MAX_LOOK_AHEAD: u64 = 0;
 
 use once_cell::sync::Lazy;
 
@@ -43,6 +45,7 @@ pub fn get_poller_block_stream(
 ) -> impl futures::Stream<Item = Vec<BlockInfo>> {
     stream! {
         start_latest_slot_updater(client.clone()).await;
+        println!("last_indexed_slot: {:?}", last_indexed_slot);
         let mut block_fetching_futures = FuturesUnordered::new();
         let mut block_cache: BTreeMap<u64, BlockInfo> = BTreeMap::new();
         let mut in_process_slots = HashSet::new();
@@ -50,16 +53,18 @@ pub fn get_poller_block_stream(
             0 => 0,
             last_indexed_slot => last_indexed_slot + 1
         };
+        let mut skipped_slot_cache = LruCache::new(NonZeroUsize::new(1000).unwrap());
 
 
         loop {
             let current_slot = LATEST_SLOT.load(Ordering::SeqCst);
+            // Refill the block fetching futures with new slots to fetch
             next_slot_to_fetch = max(next_slot_to_fetch, last_indexed_slot + 1);
             for _ in 0..max_concurrent_block_fetches {
                 if next_slot_to_fetch > current_slot + MAX_LOOK_AHEAD {
                     break;
                 }
-                if is_slot_unprocessed(next_slot_to_fetch, &in_process_slots, &block_cache, last_indexed_slot) {
+                if !skipped_slot_cache.contains(&next_slot_to_fetch) && is_slot_unprocessed(next_slot_to_fetch, &in_process_slots, &block_cache, last_indexed_slot) {
                     block_fetching_futures.push(fetch_block(
                         client.uri.clone(),
                         next_slot_to_fetch,
@@ -69,20 +74,26 @@ pub fn get_poller_block_stream(
                 }
                 next_slot_to_fetch += 1;
             }
-
             while let Some(block) = block_fetching_futures.next().await {
                 let (block_result, slot) = block;
                 in_process_slots.remove(&slot);
                 if let Ok(block) = block_result {
+                    if let None = block {
+                        skipped_slot_cache.push(slot, true);
+                    }
+                    let block = block.unwrap();
+
                     let current_slot = LATEST_SLOT.load(Ordering::SeqCst);
-                    for next_slot in (block.metadata.slot + 1)..(block.metadata.slot + 1 + max_concurrent_block_fetches as u64) {
+
+                    // Refill the block fetching futures with new slots to fetch
+                    for next_slot in (block.metadata.slot + 1 )..(block.metadata.slot + 1 + max_concurrent_block_fetches as u64) {
                         if in_process_slots.len() >= max_concurrent_block_fetches {
                             break;
                         }
                         if next_slot > current_slot + MAX_LOOK_AHEAD {
                             break;
                         }
-                        if is_slot_unprocessed(next_slot, &in_process_slots, &block_cache, last_indexed_slot) {
+                        if !skipped_slot_cache.contains(&next_slot) && is_slot_unprocessed(next_slot, &in_process_slots, &block_cache, last_indexed_slot) {
                             block_fetching_futures.push(fetch_block(
                                 client.uri.clone(),
                                 next_slot,
@@ -91,6 +102,8 @@ pub fn get_poller_block_stream(
                             in_process_slots.insert(next_slot);
                         }
                     }
+
+                    // If the block is the next block to index, emit it and the consecutive blocks in the block cache
                     if block.metadata.slot == 0 || block.metadata.parent_slot == last_indexed_slot {
                         last_indexed_slot = block.metadata.slot;
                         let mut blocks_to_index = vec![block];
@@ -105,6 +118,9 @@ pub fn get_poller_block_stream(
                     }
                     else {
                         let parent_slot = block.metadata.parent_slot;
+                        let slot = block.metadata.slot;
+
+                        // If the parent block is not processed, fetch it
                         if is_slot_unprocessed(parent_slot, &in_process_slots, &block_cache, last_indexed_slot) {
                             block_fetching_futures.push(fetch_block(
                                 client.uri.clone(),
@@ -115,14 +131,15 @@ pub fn get_poller_block_stream(
                         }
                         block_cache.insert(block.metadata.slot, block.clone());
 
-                        for next_slot in (parent_slot + 1)..(parent_slot + 1 + max_concurrent_block_fetches as u64) {
+                        // Refill the block fetching futures with new slots to fetch
+                        for next_slot in (last_indexed_slot + 1)..(slot + 1 + max_concurrent_block_fetches as u64) {
                             if in_process_slots.len() >= max_concurrent_block_fetches {
                                 break;
                             }
                             if next_slot > current_slot + MAX_LOOK_AHEAD {
                                 break;
                             }
-                            if is_slot_unprocessed(next_slot, &in_process_slots, &block_cache, last_indexed_slot) {
+                            if !skipped_slot_cache.contains(&next_slot) && is_slot_unprocessed(next_slot, &in_process_slots, &block_cache, last_indexed_slot) {
                                 block_fetching_futures.push(fetch_block(
                                     client.uri.clone(),
                                     next_slot,
@@ -136,6 +153,8 @@ pub fn get_poller_block_stream(
                     }
                 }
             }
+            // Sleep to give the chance to other thread to update LATEST_SLOT
+            sleep(Duration::from_millis(10));
 
         }
     }
@@ -168,7 +187,10 @@ fn pop_cached_blocks_to_index(
             blocks.push(block.clone());
         } else {
             if min_slot < last_indexed_slot {
-                panic!("Block is not indexed: {}", min_slot);
+                panic!(
+                    "Block is smaller than last indexed slot: {} < {}",
+                    min_slot, last_indexed_slot
+                );
             }
             break;
         }
@@ -195,7 +217,7 @@ pub async fn fetch_block(
     rpc_uri: String,
     slot: u64,
     retries: u64,
-) -> (Result<BlockInfo, client_error::ClientError>, u64) {
+) -> (Result<Option<BlockInfo>, client_error::ClientError>, u64) {
     let mut attempt_counter = 0;
     loop {
         let timeout_sec = if attempt_counter == 0 { 3 } else { 20 };
@@ -221,7 +243,10 @@ pub async fn fetch_block(
                 metric! {
                     statsd_count!("rpc_block_fetched", 1);
                 }
-                return (Ok(parse_ui_confirmed_blocked(block, slot).unwrap()), slot);
+                return (
+                    Ok(Some(parse_ui_confirmed_blocked(block, slot).unwrap())),
+                    slot,
+                );
             }
             Err(e) => {
                 if let solana_client::client_error::ClientErrorKind::RpcError(
@@ -240,7 +265,7 @@ pub async fn fetch_block(
                             }
                             log::info!("Skipped block: {}", slot);
                         }
-                        return (Err(e), slot);
+                        return (Ok(None), slot);
                     }
                 }
                 log::debug!("Failed to fetch block: {}. {}", slot, e.to_string());
