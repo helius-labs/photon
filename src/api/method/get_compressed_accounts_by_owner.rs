16ffb41fed10024785673bd64e01a4b9f71084b1
Pedro Mantica <pedro@helius.xyz>
2025-01-22 19:51:28 +0000

    Nit

--- src/api/method/get_compressed_accounts_by_owner.rs
+++ src/api/method/get_compressed_accounts_by_owner.rs
@@ -115,7 +115,7 @@ pub async fn get_compressed_accounts_by_owner(
 
     let owner_string = bytes_to_sql_format(conn.get_database_backend(), owner.into());
 
-    if filters.is_empty() && !SOL_LAYER_ACCOUNTS.contains(&owner_string.as_str()) {
+    if !filters.is_empty() && !SOL_LAYER_ACCOUNTS.contains(&owner_string.as_str()) {
         let raw_sql = format!(
             "
             SELECT CASE
