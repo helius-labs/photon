6c16f80598c7103c1a3399347eefdb8e4c1fcc5c
Linus Kendall <git@linuskendall.com>
2025-01-28 03:22:23 +0000

    Add monitoring metrics
    - Track the number of root hash failures
    - Turn success metric into a gauge
    - Report 0/1 on the success metric

--- src/monitor/mod.rs
+++ src/monitor/mod.rs
@@ -245,12 +245,16 @@ async fn validate_tree_roots(rpc_client: &RpcClient, db_roots: Vec<(Pubkey, Hash
                         db_hash,
                         account_roots
                     );
-                    return;
+                    metric! {
+                        statsd_count!("root_validation_failures", 1);
+                        statsd_gauge!("root_validation_success", 0);
+                    }
+                    return; 
                 }
             }
         }
     }
     metric! {
-        statsd_count!("root_validation_success", 1);
+        statsd_gauge!("root_validation_success", 1);
     }
 }
