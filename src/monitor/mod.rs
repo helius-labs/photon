a87336869b0d6ae67e85972c9a3ff71ec658c4a7
bruswejn <mateuszmroz1986@gmail.com>
2025-01-29 13:50:02 +0100

    non existent metric root_validation_failures

--- src/monitor/mod.rs
+++ src/monitor/mod.rs
@@ -202,6 +202,7 @@ async fn validate_tree_roots(rpc_client: &RpcClient, db_roots: Vec<(Pubkey, Hash
     } else {
         metric! {
             statsd_gauge!("root_validation_success", 1);
+            statsd_gauge!("root_validation_failures", 0);
         }
     }
 }
