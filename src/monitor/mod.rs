95f06663aadd07bf6b3a9e8c5d836622de39e357
bruswejn <mateuszmroz1986@gmail.com>
2025-01-29 13:53:47 +0100

    fix to count

--- src/monitor/mod.rs
+++ src/monitor/mod.rs
@@ -202,7 +202,7 @@ async fn validate_tree_roots(rpc_client: &RpcClient, db_roots: Vec<(Pubkey, Hash
     } else {
         metric! {
             statsd_gauge!("root_validation_success", 1);
-            statsd_gauge!("root_validation_failures", 0);
+            statsd_count!("root_validation_failures", 0);
         }
     }
 }
