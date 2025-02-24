8733c590db05431db2a47e69381067da344e1c2f
Sergey Timoshin <sergeytimoshin@proton.me>
2025-02-24 23:32:25 +0000

    feat: add queue and tree_type fields to mock test data

--- tests/integration_tests/mock_tests.rs
+++ tests/integration_tests/mock_tests.rs
@@ -571,6 +571,8 @@ async fn test_persist_token_data(
             discriminator: Set(Some(Decimal::from(1))),
             data_hash: Set(Some(Hash::new_unique().to_vec())),
             tree: Set(Pubkey::new_unique().to_bytes().to_vec()),
+            queue: Set(Pubkey::new_unique().to_bytes().to_vec()),
+            tree_type: Set(TreeType::State as i32),
             seq: Set(Some(0)),
             ..Default::default()
         };
