07242cf186a56c431739d54000a2ba02255e4ea0
bruswejn <mateuszmroz1986@gmail.com>
2025-05-28 11:53:29 +0200

    add getSlot for healthcheck

--- src/api/rpc_server.rs
+++ src/api/rpc_server.rs
@@ -213,6 +213,12 @@ fn build_rpc_module(api_and_indexer: PhotonApi) -> Result<RpcModule<PhotonApi>, 
         },
     )?;
 
+    // Alias for getIndexerSlot, to enable compatibility with health check
+    module.register_async_method("getSlot", |_rpc_params, rpc_context| async move {
+        let api = rpc_context.as_ref();
+        api.get_indexer_slot().await.map_err(Into::into)
+    })?;
+
     module.register_async_method(
         "getCompressedAccountsByOwner",
         |rpc_params, rpc_context| async move {
