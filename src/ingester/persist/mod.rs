e3209b99ee82fbe0decaf3b1ef3ef98f6be7c60b
Sergey Timoshin <sergeytimoshin@proton.me>
2025-06-26 10:20:05 -0700

    Update src/ingester/persist/mod.rs
    Co-authored-by: ananas-block <58553958+ananas-block@users.noreply.github.com>

--- src/ingester/persist/mod.rs
+++ src/ingester/persist/mod.rs
@@ -434,7 +434,7 @@ async fn append_output_accounts(
             slot_created: Set(account.account.slot_created.0 as i64),
             seq: Set(account.account.seq.map(|x| x.0 as i64)),
             prev_spent: Set(None),
-            tx_hash: Set(account.context.tx_hash.as_ref().map(|x| x.to_vec())),
+            tx_hash: Default::default(), // tx hashes are only set when an account is an input
         });
 
         if let Some(token_data) = parse_token_data(&account.account)? {
