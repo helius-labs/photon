ae6b5aa4f08e1199a490dbb65e0652e7d4cc9b7e
Sergey Timoshin <timoshin.sergey@gmail.com>
2025-11-21 18:37:37 +0000

    remove grpc

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR")?);
-
-    tonic_prost_build::configure()
-        .file_descriptor_set_path(out_dir.join("photon_descriptor.bin"))
-        .compile_protos(&["proto/photon.proto"], &["proto"])?;
-
-    Ok(())
-}
