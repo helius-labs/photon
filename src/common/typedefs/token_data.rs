b095493ccf8a68cb9ac259c6f135992873d1338e
Sergey Timoshin <timoshin.sergey@gmail.com>
2026-01-09 13:36:34 +0000

    fix: parse TokenExtension TLV data

--- src/common/typedefs/token_data.rs
+++ src/common/typedefs/token_data.rs
@@ -30,9 +30,7 @@ pub enum AccountState {
     frozen,
 }
 
-#[derive(
-    Debug, PartialEq, Eq, BorshDeserialize, BorshSerialize, Clone, ToSchema, Serialize, Default,
-)]
+#[derive(Debug, PartialEq, Eq, BorshSerialize, Clone, ToSchema, Serialize, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct TokenData {
     /// The mint associated with this account
@@ -46,6 +44,42 @@ pub struct TokenData {
     pub delegate: Option<SerializablePubkey>,
     /// The account's state
     pub state: AccountState,
-    /// Placeholder for TokenExtension tlv data (unimplemented)
+    /// TokenExtension TLV data
     pub tlv: Option<Base64String>,
+}
+
+impl TokenData {
+    pub fn parse(data: &[u8]) -> Result<Self, std::io::Error> {
+        let mut buf = data;
+
+        let mint = SerializablePubkey::deserialize(&mut buf)?;
+        let owner = SerializablePubkey::deserialize(&mut buf)?;
+        let amount = UnsignedInteger::deserialize(&mut buf)?;
+        let delegate = Option::<SerializablePubkey>::deserialize(&mut buf)?;
+        let state = AccountState::deserialize(&mut buf)?;
+
+        let tlv = if buf.is_empty() {
+            None
+        } else {
+            let option_tag = buf[0];
+            buf = &buf[1..];
+
+            if option_tag == 0 {
+                None
+            } else if option_tag == 1 && !buf.is_empty() {
+                Some(Base64String(buf.to_vec()))
+            } else {
+                None
+            }
+        };
+
+        Ok(TokenData {
+            mint,
+            owner,
+            amount,
+            delegate,
+            state,
+            tlv,
+        })
+    }
 }
