71def941719c7cceedcaf33edf12e7b8d5e25c21
Sergey Timoshin <timoshin.sergey@gmail.com>
2026-03-03 17:44:43 +0000

    refactor: remove parse_account_discriminator function and update usages for clarity

--- src/common/typedefs/account/v1.rs
+++ src/common/typedefs/account/v1.rs
@@ -1,5 +1,5 @@
 use crate::api::error::PhotonApiError;
-use crate::api::method::utils::{parse_account_discriminator, parse_decimal};
+use crate::api::method::utils::parse_decimal;
 use crate::common::typedefs::bs64_string::Base64String;
 use crate::common::typedefs::hash::Hash;
 use crate::common::typedefs::serializable_pubkey::SerializablePubkey;
@@ -75,7 +75,7 @@ impl TryFrom<Model> for Account {
     type Error = PhotonApiError;
 
     fn try_from(account: Model) -> Result<Self, Self::Error> {
-        let parsed_discriminator = parse_account_discriminator(account.discriminator.clone())?;
+        let parsed_discriminator = account.discriminator.map(parse_decimal).transpose()?;
         let data = match (account.data, account.data_hash, parsed_discriminator) {
             (Some(data), Some(data_hash), Some(discriminator)) => Some(AccountData {
                 data: Base64String(data),
