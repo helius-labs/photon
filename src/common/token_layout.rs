661ace867627119e922ed387294d432af2bd5e8a
Sergey Timoshin <timoshin.sergey@gmail.com>
2026-03-03 17:38:02 +0000

    refactor: remove unused backfill migration for mint onchain pubkey

--- src/common/token_layout.rs
+++ src/common/token_layout.rs
@@ -4,8 +4,6 @@
 pub const SPL_TOKEN_ACCOUNT_BASE_LEN: usize = 165;
 /// Account type marker byte offset in token/mint account data.
 pub const TOKEN_ACCOUNT_TYPE_OFFSET: usize = SPL_TOKEN_ACCOUNT_BASE_LEN;
-/// 1-indexed equivalent for SQL substring functions.
-pub const TOKEN_ACCOUNT_TYPE_OFFSET_SQL: usize = TOKEN_ACCOUNT_TYPE_OFFSET + 1;
 
 /// AccountType::Mint discriminator value.
 pub const ACCOUNT_TYPE_MINT: u8 = 1;
@@ -14,5 +12,3 @@ pub const ACCOUNT_TYPE_MINT: u8 = 1;
 pub const COMPRESSED_MINT_PDA_OFFSET: usize = 84;
 pub const COMPRESSED_MINT_PDA_LEN: usize = 32;
 pub const COMPRESSED_MINT_PDA_END: usize = COMPRESSED_MINT_PDA_OFFSET + COMPRESSED_MINT_PDA_LEN;
-/// 1-indexed equivalent for SQL substring functions.
-pub const COMPRESSED_MINT_PDA_OFFSET_SQL: usize = COMPRESSED_MINT_PDA_OFFSET + 1;
